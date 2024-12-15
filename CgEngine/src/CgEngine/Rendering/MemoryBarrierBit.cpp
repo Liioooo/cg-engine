b0386371f20dce53a51d2f2c39129592cbed65a2
Setscha <tobias.seczer@gmail.com>
2024-12-15 14:44:45 +0100

    Fix the utter disaster Leo caused that has completely obliterated HOURS of our work for NOTHING!

--- CgEngine/src/CgEngine/Rendering/MemoryBarrierBit.cpp
+++ CgEngine/src/CgEngine/Rendering/MemoryBarrierBit.cpp
@@ -5,7 +5,7 @@ namespace CgEngine {
     unsigned int MemoryBarrierUtils::convertToBitfield(const std::initializer_list<MemoryBarrierBit>& barriers) {
         unsigned int barrier = 0;
         for (const auto& b: barriers) {
-            barrier &= static_cast<unsigned int>(b);
+            barrier |= static_cast<unsigned int>(b);
         }
         return barrier;
     }
